f4d1e19e88a5cc6fe0f4f99c70fb885d3b514987
Rone <triduyphanwork123@gmail.com>
2024-04-26 21:15:15 +0700

    add TC1_2

--- 70_TriDuy_N1_Coursera/Form1.Designer.cs
+++ 70_TriDuy_N1_Coursera/Form1.Designer.cs
@@ -30,27 +30,44 @@ namespace _70_TriDuy_N1_Coursera
         private void InitializeComponent()
         {
             this.btTestCase1_1 = new System.Windows.Forms.Button();
+            this.btTestCase1_2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btTestCase1_1
             // 
             this.btTestCase1_1.AutoSize = true;
             this.btTestCase1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btTestCase1_1.Location = new System.Drawing.Point(82, 105);
+            this.btTestCase1_1.Location = new System.Drawing.Point(82, 103);
+            this.btTestCase1_1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btTestCase1_1.Name = "btTestCase1_1";
-            this.btTestCase1_1.Size = new System.Drawing.Size(140, 44);
+            this.btTestCase1_1.Size = new System.Drawing.Size(373, 105);
             this.btTestCase1_1.TabIndex = 0;
             this.btTestCase1_1.Text = "TestCase1_1";
             this.btTestCase1_1.UseVisualStyleBackColor = true;
             this.btTestCase1_1.Click += new System.EventHandler(this.btTestCase1_1_Click);
             // 
+            // btTestCase1_2
+            // 
+            this.btTestCase1_2.AutoSize = true;
+            this.btTestCase1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase1_2.Location = new System.Drawing.Point(82, 336);
+            this.btTestCase1_2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btTestCase1_2.Name = "btTestCase1_2";
+            this.btTestCase1_2.Size = new System.Drawing.Size(373, 105);
+            this.btTestCase1_2.TabIndex = 1;
+            this.btTestCase1_2.Text = "TestCase1_2";
+            this.btTestCase1_2.UseVisualStyleBackColor = true;
+            this.btTestCase1_2.Click += new System.EventHandler(this.btTestCase1_2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1590, 800);
+            this.ClientSize = new System.Drawing.Size(2968, 1662);
+            this.Controls.Add(this.btTestCase1_2);
             this.Controls.Add(this.btTestCase1_1);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -61,6 +78,7 @@ namespace _70_TriDuy_N1_Coursera
         #endregion
 
         private System.Windows.Forms.Button btTestCase1_1;
+        private System.Windows.Forms.Button btTestCase1_2;
     }
 }
 
--- 70_TriDuy_N1_Coursera/Form1.cs
+++ 70_TriDuy_N1_Coursera/Form1.cs
@@ -12,6 +12,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 
+
 namespace _70_TriDuy_N1_Coursera
 {
     public partial class Form1 : Form
@@ -22,20 +23,34 @@ namespace _70_TriDuy_N1_Coursera
         }
 
 
-        private void btTestCase1_1_Click(object sender, EventArgs e)
+        private void btTestCase1_1_Click(object sender, EventArgs e) //TestCase1: Nhập thiếu họ và tên người dùng khi đăng ký thành viên
         {
             IWebDriver Duy_70 = new ChromeDriver();
             string email = "supertun819@hotmail.com";
             string pass = "123456@hihi";
             Duy_70.Navigate().GoToUrl("https://www.coursera.org/");
-            Duy_70.FindElement(By.LinkText("Join for Free")).Click();
-            Duy_70.FindElement(By.Id("email")).SendKeys(email);
-            Duy_70.FindElement(By.Id("password")).SendKeys(pass);
+            Duy_70.FindElement(By.CssSelector("#rendered-content > div > main > section.rc-HeroBannerRedesign.css-rpf906 > div > div.css-je23cs > button > span")).Click(); // chuyển sang trang đăng ký
+            //Duy_70.FindElement(By.Id("email")).SendKeys(email);
+            Duy_70.FindElement(By.XPath("//*[@id=\"email\"]")).SendKeys(email); // nhập thông tin email
+            Duy_70.FindElement(By.Id("password")).SendKeys(pass); // nhập thông tin password
+            Duy_70.FindElement(By.CssSelector("button[type='submit']")).Click(); // ấn button Join for Free để hoàn thành việc đăng ký, sử dụng CssSelector
+            Thread.Sleep(3000); // cho chương trình dừng lại 3s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+        private void btTestCase1_2_Click(object sender, EventArgs e) //TestCase2: Nhập thiếu email người dùng khi đăng ký thành viên
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string name = "Phan Tri Duy";
+            string pass = "123456@hihi";
+            Duy_70.Navigate().GoToUrl("https://www.coursera.org/");
+            Duy_70.FindElement(By.CssSelector("button[class^='cds-149']")).Click(); //chuyển sang trang đăng ky, tìm tag, attribute với kí tự đầu của chuỗi
+            //Duy_70.FindElement(By.Name("name")).SendKeys(name);
+            Duy_70.FindElement(By.CssSelector("[type='text'][placeholder='Enter your full name']")).SendKeys(name); //nhập thông tin họ và tên, sử dụng CssSelector kết hợp giữa thuộc tính type và placeholder
+            Duy_70.FindElement(By.CssSelector("input#password")).SendKeys(pass); //nhập mật khẩu, sử dụng CssSelector Tag và ID
             Duy_70.FindElement(By.CssSelector("button[type='submit']")).Click();
-            Thread.Sleep(3000);
-            //ChromeDriverService D70 = ChromeDriverService.CreateDefaultService();
-            //D70.HideCommandPromptWindow = true;
-            Duy_70.Quit();
+            Thread.Sleep(3000); // cho chương trình dừng lại 3s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
         }
     }
 }
