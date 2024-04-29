d59b83fa3e4c750188392544d39776d69a44e6c5
CullenRone <triduyphanwork123@gmail.com>
2024-04-29 18:00:40 +0700

    change sub and TC1_1-6

---
+++ 70_TriDuy_N1_Yody/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace _70_TriDuy_N1_Yody
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btTestCase1_1 = new System.Windows.Forms.Button();
+            this.btTestCase1_2 = new System.Windows.Forms.Button();
+            this.btTestCase1_3 = new System.Windows.Forms.Button();
+            this.btTestCase1_4 = new System.Windows.Forms.Button();
+            this.btTestCase1_5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btTestCase2_1 = new System.Windows.Forms.Button();
+            this.btTestCase2_2 = new System.Windows.Forms.Button();
+            this.btTestCase1_6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btTestCase1_1
+            // 
+            this.btTestCase1_1.AutoSize = true;
+            this.btTestCase1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase1_1.Location = new System.Drawing.Point(31, 43);
+            this.btTestCase1_1.Name = "btTestCase1_1";
+            this.btTestCase1_1.Size = new System.Drawing.Size(140, 42);
+            this.btTestCase1_1.TabIndex = 0;
+            this.btTestCase1_1.Text = "TestCase1_1";
+            this.btTestCase1_1.UseVisualStyleBackColor = true;
+            this.btTestCase1_1.Click += new System.EventHandler(this.btTestCase1_1_Click);
+            // 
+            // btTestCase1_2
+            // 
+            this.btTestCase1_2.AutoSize = true;
+            this.btTestCase1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase1_2.Location = new System.Drawing.Point(31, 141);
+            this.btTestCase1_2.Name = "btTestCase1_2";
+            this.btTestCase1_2.Size = new System.Drawing.Size(140, 44);
+            this.btTestCase1_2.TabIndex = 1;
+            this.btTestCase1_2.Text = "TestCase1_2";
+            this.btTestCase1_2.UseVisualStyleBackColor = true;
+            this.btTestCase1_2.Click += new System.EventHandler(this.btTestCase1_2_Click);
+            // 
+            // btTestCase1_3
+            // 
+            this.btTestCase1_3.AutoSize = true;
+            this.btTestCase1_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase1_3.Location = new System.Drawing.Point(31, 244);
+            this.btTestCase1_3.Name = "btTestCase1_3";
+            this.btTestCase1_3.Size = new System.Drawing.Size(140, 44);
+            this.btTestCase1_3.TabIndex = 2;
+            this.btTestCase1_3.Text = "TestCase1_3";
+            this.btTestCase1_3.UseVisualStyleBackColor = true;
+            this.btTestCase1_3.Click += new System.EventHandler(this.btTestCase1_3_Click);
+            // 
+            // btTestCase1_4
+            // 
+            this.btTestCase1_4.AutoSize = true;
+            this.btTestCase1_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase1_4.Location = new System.Drawing.Point(31, 339);
+            this.btTestCase1_4.Name = "btTestCase1_4";
+            this.btTestCase1_4.Size = new System.Drawing.Size(140, 44);
+            this.btTestCase1_4.TabIndex = 3;
+            this.btTestCase1_4.Text = "TestCase1_4";
+            this.btTestCase1_4.UseVisualStyleBackColor = true;
+            this.btTestCase1_4.Click += new System.EventHandler(this.btTestCase1_4_Click);
+            // 
+            // btTestCase1_5
+            // 
+            this.btTestCase1_5.AutoSize = true;
+            this.btTestCase1_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase1_5.Location = new System.Drawing.Point(31, 440);
+            this.btTestCase1_5.Name = "btTestCase1_5";
+            this.btTestCase1_5.Size = new System.Drawing.Size(140, 44);
+            this.btTestCase1_5.TabIndex = 4;
+            this.btTestCase1_5.Text = "TestCase1_5";
+            this.btTestCase1_5.UseVisualStyleBackColor = true;
+            this.btTestCase1_5.Click += new System.EventHandler(this.btTestCase1_5_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 490);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btTestCase2_1
+            // 
+            this.btTestCase2_1.AutoSize = true;
+            this.btTestCase2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase2_1.Location = new System.Drawing.Point(329, 43);
+            this.btTestCase2_1.Name = "btTestCase2_1";
+            this.btTestCase2_1.Size = new System.Drawing.Size(140, 44);
+            this.btTestCase2_1.TabIndex = 7;
+            this.btTestCase2_1.Text = "TestCase2_1";
+            this.btTestCase2_1.UseVisualStyleBackColor = true;
+            this.btTestCase2_1.Click += new System.EventHandler(this.btTestCase2_1_Click);
+            // 
+            // btTestCase2_2
+            // 
+            this.btTestCase2_2.AutoSize = true;
+            this.btTestCase2_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase2_2.Location = new System.Drawing.Point(329, 141);
+            this.btTestCase2_2.Name = "btTestCase2_2";
+            this.btTestCase2_2.Size = new System.Drawing.Size(140, 44);
+            this.btTestCase2_2.TabIndex = 8;
+            this.btTestCase2_2.Text = "TestCase2_2";
+            this.btTestCase2_2.UseVisualStyleBackColor = true;
+            this.btTestCase2_2.Click += new System.EventHandler(this.btTestCase2_2_Click);
+            // 
+            // btTestCase1_6
+            // 
+            this.btTestCase1_6.AutoSize = true;
+            this.btTestCase1_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTestCase1_6.Location = new System.Drawing.Point(31, 540);
+            this.btTestCase1_6.Name = "btTestCase1_6";
+            this.btTestCase1_6.Size = new System.Drawing.Size(140, 44);
+            this.btTestCase1_6.TabIndex = 9;
+            this.btTestCase1_6.Text = "TestCase1_6";
+            this.btTestCase1_6.UseVisualStyleBackColor = true;
+            this.btTestCase1_6.Click += new System.EventHandler(this.btTestCase1_6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(962, 596);
+            this.Controls.Add(this.btTestCase1_6);
+            this.Controls.Add(this.btTestCase2_2);
+            this.Controls.Add(this.btTestCase2_1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btTestCase1_5);
+            this.Controls.Add(this.btTestCase1_4);
+            this.Controls.Add(this.btTestCase1_3);
+            this.Controls.Add(this.btTestCase1_2);
+            this.Controls.Add(this.btTestCase1_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btTestCase1_1;
+        private System.Windows.Forms.Button btTestCase1_2;
+        private System.Windows.Forms.Button btTestCase1_3;
+        private System.Windows.Forms.Button btTestCase1_4;
+        private System.Windows.Forms.Button btTestCase1_5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btTestCase2_1;
+        private System.Windows.Forms.Button btTestCase2_2;
+        private System.Windows.Forms.Button btTestCase1_6;
+    }
+}
+
---
+++ 70_TriDuy_N1_Yody/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace _70_TriDuy_N1_Yody
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //Chức năng đăng ký người dùng ( 6 TestCase ) 70_PhanDuy_N1_Yody
+
+        private void btTestCase1_1_Click(object sender, EventArgs e) //TestCase1: Nhập thiếu họ và tên người dùng khi đăng ký thành viên
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string sdt = "0983293706";
+            string email = "supertun819@hotmail.com";
+            string pass = "123456@hihi";
+            Duy_70.Navigate().GoToUrl("https://yody.vn/");
+            Duy_70.FindElement(By.LinkText("ĐĂNG KÝ")).Click(); //chuyển sang trang đăng ký 
+            Duy_70.FindElement(By.XPath("//*[@id=\"phone\"]")).SendKeys(sdt); // nhập thông tin sđt
+            Duy_70.FindElement(By.Id("iptEmail")).SendKeys(email); // nhập thông tin email
+            Duy_70.FindElement(By.Id("password")).SendKeys(pass); // nhập thông tin password
+            Duy_70.FindElement(By.CssSelector("#btnSubmit")).Click(); // ấn button Đăng ký để hoàn thành việc đăng ký 
+            //Duy_70.FindElement(By.CssSelector("button[type='submit']")).Click(); // ấn button Đăng ký để hoàn thành việc đăng ký
+            Thread.Sleep(5000); // cho chương trình dừng lại 5s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+        private void btTestCase1_2_Click(object sender, EventArgs e) //TestCase2: Nhập thiếu số điện thoại người dùng khi đăng ký thành viên
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string name = "Phan Duy";
+            string email = "supertun819@hotmail.com";
+            string pass = "123456@hihi";
+            Duy_70.Navigate().GoToUrl("https://yody.vn/");
+            //Duy_70.FindElement(By.LinkText("ĐĂNG KÝ")).Click(); //chuyển sang trang đăng ký
+            Duy_70.FindElement(By.CssSelector("a[class ^='regis']")).Click(); //chuyển sang trang đăng ky, tìm tag, attribute với kí tự đầu của chuỗi
+            Duy_70.FindElement(By.Name("first_name")).SendKeys(name); //nhập thông tin họ và tên 
+            Duy_70.FindElement(By.CssSelector("[type='text'][placeholder='Địa chỉ email']")).SendKeys(email); //nhập thông tin email, sử dụng CssSelector kết hợp giữa thuộc tính type và placeholder
+            Duy_70.FindElement(By.CssSelector("input#password")).SendKeys(pass); //nhập mật khẩu, sử dụng CssSelector Tag và ID
+            Duy_70.FindElement(By.XPath("//*[@id=\"btnSubmit\"]")).Click(); // ấn button Đăng ký để hoàn thành việc đăng ký
+            Thread.Sleep(5000); // cho chương trình dừng lại 5s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+        private void btTestCase1_3_Click(object sender, EventArgs e) //TestCase3: Nhập thiếu email người dùng khi đăng ký thành viên
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string name = "Phan Duy";
+            string sdt = "0983293706";
+            string pass = "123456@hihi";
+            Duy_70.Navigate().GoToUrl("https://yody.vn/");
+            Duy_70.FindElement(By.CssSelector("a[class ^='regis']")).Click(); //chuyển sang trang đăng ky, tìm tag, attribute với kí tự đầu của chuỗi
+            Duy_70.FindElement(By.Name("first_name")).SendKeys(name); // nhập họ và tên người dùng
+            Duy_70.FindElement(By.Id("phone")).SendKeys(sdt); // nhập số điện thoại người dùng
+            Duy_70.FindElement(By.CssSelector("input#password")).SendKeys(pass); //nhập mật khẩu, sử dụng CssSelector Tag và ID
+            Duy_70.FindElement(By.XPath("//*[@id=\"btnSubmit\"]")).Click(); // ấn button Đăng ký để hoàn thành việc đăng ký
+            Thread.Sleep(3000); // cho chương trình dừng lại 3s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+        private void btTestCase1_4_Click(object sender, EventArgs e) //TestCase4: Nhập thiếu mật khẩu người dùng khi đăng ký thành viên
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string name = "Phan Duy";
+            string sdt = "0983293706";
+            string email = "supertun819@hotmail.com";
+            Duy_70.Navigate().GoToUrl("https://yody.vn/");
+            Duy_70.FindElement(By.LinkText("ĐĂNG KÝ")).Click(); //chuyển sang trang đăng ký
+            Duy_70.FindElement(By.Name("first_name")).SendKeys(name); // nhập họ và tên người dùng
+            Duy_70.FindElement(By.XPath("//*[@id=\"phone\"]")).SendKeys(sdt); // nhập thông tin sđt
+            Duy_70.FindElement(By.CssSelector("input#iptEmail")).SendKeys(email); //nhập email, sử dụng CssSelector Tag và ID 
+            Duy_70.FindElement(By.CssSelector("button[class ^= 'btn-login'")).Click(); // ấn button Đăng ký để hoàn thành việc đăng ký
+            Thread.Sleep(5000); // cho chương trình dừng lại 5s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+        private void btTestCase1_5_Click(object sender, EventArgs e) //TestCase5: Mật khẩu không đủ độ dài kí tự 
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string name = "Phan Duy";
+            string sdt = "0983293706";
+            string email = "supertun819@hotmail.com";
+            string pass = "1234";
+            Duy_70.Navigate().GoToUrl("https://yody.vn/");
+            Duy_70.FindElement(By.PartialLinkText("ĐĂNG")).Click(); //chuyển sang trang đăng ký
+            Duy_70.FindElement(By.Id("first_name")).SendKeys(name); // nhập họ và tên người dùng
+            Duy_70.FindElement(By.CssSelector("[type='text'][placeholder='Số điện thoại']")).SendKeys(sdt); // nhập số điện thoại người dùng
+            Duy_70.FindElement(By.Id("iptEmail")).SendKeys(email); // nhập thông tin email
+            Duy_70.FindElement(By.CssSelector("input#password")).SendKeys(pass); //nhập mật khẩu, sử dụng CssSelector Tag và ID
+            Duy_70.FindElement(By.XPath("//*[@id=\"btnSubmit\"]")).Click(); // ấn button Đăng ký để hoàn thành việc đăng ký
+            Thread.Sleep(5000); // cho chương trình dừng lại 5s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+        private void btTestCase1_6_Click(object sender, EventArgs e)
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string name = "Phan Duy";
+            string sdt = "0983293706";
+            string email = "supertun819@hotmail.com";
+            string pass = "123456@hihi";
+            Duy_70.Navigate().GoToUrl("https://yody.vn/");
+            Duy_70.FindElement(By.XPath("/html/body/div[2]/div[3]/div/div/div[2]/div[2]/div[3]/div[1]/div/a[1]")).Click(); //chuyển sang trang đăng ký
+            Duy_70.FindElement(By.CssSelector("input[name=first_name]")).SendKeys(name); //nhập họ và tên, sử dụng CssSelector Tag, Attribute và Value
+            Duy_70.FindElement(By.CssSelector("input[class *= 'form-control-comment']")).SendKeys(sdt); //nhập số điện thoại người dùng
+            Duy_70.FindElement(By.Id("iptEmail")).SendKeys(email); // nhập thông tin email
+            Duy_70.FindElement(By.Id("password")).SendKeys(pass); // nhập thông tin password
+            Duy_70.FindElement(By.XPath("//*[@type ='submit' and @id = 'btnSubmit']")).Click(); // ấn button Đăng ký để hoàn thành việc đăng ký
+            Thread.Sleep(5000); // cho chương trình dừng lại 5s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+
+
+        //Chức năng đăng nhập người dùng ( 4 TestCase ) 70_PhanDuy_N1_Yody
+        private void btTestCase2_1_Click(object sender, EventArgs e) // TestCase 1: Nhập sai tài khoản và mật khẩu
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string email = "superjoonior@gmail.com";
+            string pass = "123456@hihi";
+            Duy_70.Navigate().GoToUrl("https://yody.vn/");
+            Duy_70.FindElement(By.LinkText("ĐĂNG NHẬP")).Click();
+            Duy_70.FindElement(By.Id("customer_email")).SendKeys(email);
+            Duy_70.FindElement(By.Id("customer_password")).SendKeys(pass);
+            Duy_70.FindElement(By.XPath("//*[@id=\"customer_login\"]/button")).Click();
+            //Duy_70.FindElement(By.LinkText("Log In")).Click(); // chuyển sang trang đăng nhập
+            //Duy_70.FindElement(By.CssSelector("[type='email'][placeholder='name@email.com']")).SendKeys(email); //nhập thông tin email, sử dụng CssSelector kết hợp giữa thuộc tính type và placeholder
+            //Duy_70.FindElement(By.Id("password")).SendKeys(pass); //nhập password
+            //Duy_70.FindElement(By.XPath("//*[@type=\"submit\"]")).Click(); // ấn button Login để hoàn thành việc đăng nhập
+            Thread.Sleep(10000); // cho chương trình dừng lại 3s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+        private void btTestCase2_2_Click(object sender, EventArgs e) // TestCase 2: Nhập đúng tài khoản nhưng sai mật khẩu
+        {
+            IWebDriver Duy_70 = new ChromeDriver();
+            string email = "supertun819@hotmail.com";
+            string pass = "123456789";
+            Duy_70.Navigate().GoToUrl("https://www.coursera.org/");
+            Duy_70.FindElement(By.PartialLinkText("Log")).Click(); // chuyển sang trang đăng nhập
+            Duy_70.FindElement(By.Name("email")).SendKeys(email); // nhập email
+            Duy_70.FindElement(By.Id("password")).SendKeys(pass); // nhập password
+            Duy_70.FindElement(By.CssSelector("button[class ^= '_6dgzsvq']")).Click(); // ấn button Login để hoàn thành việc đăng nhập
+            Thread.Sleep(3000); // cho chương trình dừng lại 3s 
+            Duy_70.Quit(); //đóng hết các trình duyệt đang mở khi hoàn thành chương trình
+        }
+
+        
+    }
+}
